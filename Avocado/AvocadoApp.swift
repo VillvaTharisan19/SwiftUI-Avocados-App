41ab9583822f47ae5ec9dbd7b584ab6c5fd6e208
Villva tharisan Ramasamy <villvatharisanramasamy@Villvas-MacBook-Pro.local>
2023-07-16 17:56:12 +0800

    Initial commit- Setup project settings & added assets files

---
+++ Avocado/AvocadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoApp.swift
+//  Avocado
+//
+//  Created by Villva tharisan Ramasamy on 16/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
