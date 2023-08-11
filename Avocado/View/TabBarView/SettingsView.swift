255c4846a70b0f6faeea0304a685d8a7df23bc50
Villva Tharisan <gvillva@gmail.com>
2023-08-11 18:15:53 +0800

    feat(basic-form): Create basic form for settings view

--- Avocado/View/TabBarView/SettingsView.swift
+++ Avocado/View/TabBarView/SettingsView.swift
@@ -8,8 +8,91 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @State private var enableNotification: Bool = true
+    @State private var backgroundRefresh: Bool = false
+    
     var body: some View {
-        Text("Settings")
+        VStack(alignment: .center, spacing: 0) {
+            VStack(alignment: .center, spacing: 5) {
+                Image("avocado")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.top)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .shadow(color: Color("ColorBlackTransparentLight"), radius: 8, x: 0, y: 4)
+                
+                Text("Avocados".uppercased())
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenAdaptive"))
+            }
+            .padding()
+            
+            Form {
+                Section(header: Text("General Settings")) {
+                    Toggle(isOn: $enableNotification) {
+                        Text("Enable Notification")
+                    }
+                    
+                    Toggle(isOn: $backgroundRefresh) {
+                        Text("Background Refresh")
+                    }
+                }
+                
+                Section(header: Text("Application")) {
+                    if enableNotification {
+                        HStack {
+                            Text("Product").foregroundColor(.gray)
+                            Spacer()
+                            Text("Avocado Recipes")
+                        }
+                        
+                        HStack {
+                            Text("Compatibility").foregroundColor(.gray)
+                            Spacer()
+                            Text("iPhone & iPad")
+                        }
+                        
+                        HStack {
+                            Text("Developer").foregroundColor(.gray)
+                            Spacer()
+                            Text("Villva Tharisan")
+                        }
+                        
+                        HStack {
+                            Text("Designer").foregroundColor(.gray)
+                            Spacer()
+                            Text("Udemy")
+                        }
+                        
+                        HStack {
+                            Text("Website").foregroundColor(.gray)
+                            Spacer()
+                            Text("udemy.com")
+                        }
+                        
+                        HStack {
+                            Text("Version").foregroundColor(.gray)
+                            Spacer()
+                            Text("1.0.0")
+                        }
+                        
+                    } else {
+                        
+                        HStack {
+                            Text("Personal Message").foregroundColor(.gray)
+                            Spacer()
+                            Text("Happy Coding!")
+                        }
+                        
+                    }
+                    
+                }
+
+            }
+        }
+        .frame(maxWidth: 640)
     }
 }
 
